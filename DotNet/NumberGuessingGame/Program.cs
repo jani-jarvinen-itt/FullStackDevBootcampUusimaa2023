1e32dd472db21d189e72e273a9a84f2f4e1d0057
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-04-25 14:43:22 +0300

    Finalized code for the game and added some source code comments as well.

--- DotNet/NumberGuessingGame/Program.cs
+++ DotNet/NumberGuessingGame/Program.cs
@@ -1,15 +1,18 @@
 ﻿Console.WriteLine("Welcome to the Number Guessing Game!");
 
+// select random number 1-20
 int correct = Random.Shared.Next(1, 21);
 Console.WriteLine(correct);
 
+// they plays has three guesses/rounds
 for (int round = 0; round < 3; round++)
 {
+    // ask the player for a guess
     Console.WriteLine("Please enter your guess between 1 and 20:");
     string guessValue = Console.ReadLine();
     int guess = int.Parse(guessValue);
 
-
+    // is the guess correct?
     if (guess == correct)
     {
         Console.WriteLine("Correct! You win the game!");
@@ -24,8 +27,5 @@ for (int round = 0; round < 3; round++)
         Console.WriteLine("The correct number is smaller.");
     }
 }
-
-
-
 
 Console.WriteLine("Game ends.");
