8a51496e93b97ddd749f87395472d93ef21b95e8
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-04-25 11:44:33 +0300

    Added C# example of string and palindrome detection.

---
+++ DotNet/PalindromeDetect/Program.cs
@@ -0,0 +1,24 @@
+﻿// read user's input
+Console.WriteLine("Please enter a string:");
+string input = Console.ReadLine();
+
+// method 1: reverse the input
+string reversed = "";
+for (int index = input.Length - 1; index >= 0; index--)
+{
+    reversed += input[index];
+}
+Console.WriteLine(reversed);
+
+// compare the input to the reversed string
+bool isPalindrome = (input == reversed);
+
+// display results
+if (isPalindrome)
+{
+    Console.WriteLine("The input is a palindrome.");
+}
+else
+{
+    Console.WriteLine("The input is NOT a palindrome.");
+}
