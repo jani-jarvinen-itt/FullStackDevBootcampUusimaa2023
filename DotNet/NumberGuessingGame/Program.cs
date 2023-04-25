d08667440c06293e9c93bc0ad1ab9403a63d0e72
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-04-25 14:28:20 +0300

    Continued game development. Added basic testing of the player's guess.

--- DotNet/NumberGuessingGame/Program.cs
+++ DotNet/NumberGuessingGame/Program.cs
@@ -3,4 +3,27 @@
 int correct = Random.Shared.Next(1, 21);
 Console.WriteLine(correct);
 
+
+Console.WriteLine("Please enter your guess between 1 and 20:");
+string guessValue = Console.ReadLine();
+int guess = int.Parse(guessValue);
+
+
+if (guess == correct)
+{
+    Console.WriteLine("Correct! You win the game!");
+}
+else if (guess < correct)
+{
+    Console.WriteLine("The correct number is larger.");
+}
+else
+{
+    Console.WriteLine("The correct number is smaller.");
+}
+
+
+
+
+
 Console.WriteLine("Game ends.");
