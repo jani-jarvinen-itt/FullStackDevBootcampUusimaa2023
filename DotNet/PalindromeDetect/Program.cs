20535443f6fe43e45e1ac2d4cbe64ea9bf608d5f
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-04-25 11:53:35 +0300

    Added second string reversing example in C#.

--- DotNet/PalindromeDetect/Program.cs
+++ DotNet/PalindromeDetect/Program.cs
@@ -3,11 +3,19 @@ Console.WriteLine("Please enter a string:");
 string input = Console.ReadLine();
 
 // method 1: reverse the input
+/*
 string reversed = "";
 for (int index = input.Length - 1; index >= 0; index--)
 {
     reversed += input[index];
 }
+Console.WriteLine(reversed);
+*/
+
+// method 2: reverse the input
+char[] chars = input.ToCharArray();
+Array.Reverse(chars);
+string reversed = new string(chars);
 Console.WriteLine(reversed);
 
 // compare the input to the reversed string
