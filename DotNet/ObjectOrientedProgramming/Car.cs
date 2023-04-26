4969c978e1521577ea4599d83ed46a92a86f33d5
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-04-26 10:44:50 +0300

    Started creating a simple OOP example with C#.

---
+++ DotNet/ObjectOrientedProgramming/Car.cs
@@ -0,0 +1,11 @@
+public class Car
+{
+    // properties
+    public string Model { get; set; }
+
+    public string Make { get; set; }
+
+    public int CurrentSpeed { get; set; }
+
+    public bool EngineRunning { get; set; }
+}
---
+++ DotNet/ObjectOrientedProgramming/Program.cs
@@ -0,0 +1,11 @@
+﻿// variable declaration: <type> <name> = <value>;
+string name = "John Doe";
+
+Car myCar = new();
+myCar.Make = "Toyota";
+
+
+
+
+Car myCousinsCar = new();
+Car myFriendsCar = new();
