cc2c92f1a815c5bd4071ab54ad965a947da410d0
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-04-26 13:54:24 +0300

    Added C# example on how to calculate a sum of numbers in a text file using the File class.

---
+++ DotNet/FileSum/Program.cs
@@ -0,0 +1,11 @@
+﻿string filename = @"C:\Bootcamp\Uusimaa\Code\Numbers.txt";
+
+IEnumerable<string> lines = File.ReadLines(filename);
+int sum = 0;
+foreach (string line in lines)
+{
+    int number = int.Parse(line);
+    sum += number; // sum = sum + number;
+}
+
+Console.WriteLine("Sum of numbers in the file is: " + sum);
