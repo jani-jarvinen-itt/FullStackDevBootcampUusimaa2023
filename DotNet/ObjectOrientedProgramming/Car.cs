9994925472768efafc1f10d9a98a4856266ff3cf
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-04-26 11:57:17 +0300

    Added error checking to acceleration and deceleration.

--- DotNet/ObjectOrientedProgramming/Car.cs
+++ DotNet/ObjectOrientedProgramming/Car.cs
@@ -5,7 +5,46 @@ public class Car
 
     public string Make { get; set; }
 
-    public int CurrentSpeed { get; }
+    public int CurrentSpeed { get; private set; }
 
-    public bool EngineRunning { get; }
+    public bool EngineRunning { get; private set; }
+
+    // methods
+    public void StartEngine()
+    {
+        EngineRunning = true;
+    }
+
+    public void StopEngine()
+    {
+        EngineRunning = false;
+    }
+
+    public void Accelerate()
+    {
+        if (!EngineRunning)
+        {
+            // return;
+            throw new InvalidOperationException("Cannot accelerate because the " +
+                "engine is not running. Please use StartEngine() first.");
+        }
+
+        const int MaxSpeed = 200;
+        CurrentSpeed += 10;
+
+        if (CurrentSpeed > MaxSpeed)
+        {
+            CurrentSpeed = MaxSpeed;
+        }
+    }
+
+    public void Decelerate()
+    {
+        CurrentSpeed -= 10;
+
+        if (CurrentSpeed < 0)
+        {
+            CurrentSpeed = 0;
+        }
+    }
 }
--- DotNet/ObjectOrientedProgramming/Program.cs
+++ DotNet/ObjectOrientedProgramming/Program.cs
@@ -4,9 +4,13 @@ string name = "John Doe";
 Car myCar = new();
 myCar.Make = "Toyota";
 myCar.Model = "Corolla";
+myCar.StartEngine();
+myCar.Accelerate();
+myCar.Accelerate();
 // myCar.CurrentSpeed = -555;
 // myCar.EngineRunning = true;
 Console.WriteLine("Current speed: " + myCar.CurrentSpeed);
+Console.WriteLine("Engine running: " + myCar.EngineRunning);
 
 
 
