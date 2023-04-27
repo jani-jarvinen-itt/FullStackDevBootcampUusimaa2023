a8d17594ff1f96f3032d824d3ac6d8af6413a91c
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-04-27 15:37:12 +0300

    Added example application for C# exception handling.

---
+++ DotNet/ExceptionHandling/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Exception testing, version with decimals.");
+
+/*
+int a = 100;
+int b = 0;
+int result = a / b; // DivideByZeroException
+Console.WriteLine(result);
+*/
+
+/*
+double a = 100.23;
+double b = 0;
+double result = a / b; // no exception!
+Console.WriteLine(result);
+*/
+
+/*
+decimal a = 100.23m;
+decimal b = 0;
+decimal result = a / b; // DivideByZeroException
+Console.WriteLine(result);
+*/
+
+try
+{
+    Console.WriteLine("Enter number 1:");
+    decimal number1 = decimal.Parse(Console.ReadLine());
+
+    Console.WriteLine("Enter number 2:");
+    decimal number2 = decimal.Parse(Console.ReadLine());
+
+    decimal result = number1 / number2;
+    Console.WriteLine(result);
+}
+catch
+{
+    // bad strategy to error handling
+    Console.WriteLine("An error occurred!");
+}
