03566feddd64d9d5b459cf2ccb6ed5f67b609a14
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-04-27 11:40:15 +0300

    Started to implement the C# application for date processing.

---
+++ DotNet/DateProcessing/Program.cs
@@ -0,0 +1,13 @@
+﻿// ask the user for two dates
+Console.WriteLine("Please enter the first date:");
+string date1Input = Console.ReadLine();
+
+Console.WriteLine("Please enter the second date:");
+string date2Input = Console.ReadLine();
+
+// convert the input strings to DateTimes
+DateTime firstDate = DateTime.Parse(date1Input);
+DateTime secondDate = DateTime.Parse(date2Input);
+
+Console.WriteLine(firstDate);
+Console.WriteLine(secondDate);
