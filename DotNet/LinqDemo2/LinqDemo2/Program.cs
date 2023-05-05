fc9b045e6f13ac0468c617159ba339278f3ed9fd
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-05 09:50:41 +0300

    Added an example of sorting and display numbers in correct order with both classical programming and C# LINQ queries.

---
+++ DotNet/LinqDemo2/LinqDemo2/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] numbers = { 5, 25, 37, 12, 48, 41, 33, 29 };
+
+// classical solution: sort and display numbers above 20
+Array.Sort(numbers);
+foreach (int number in numbers)
+{
+    if (number > 20)
+    {
+        Console.WriteLine(number);
+    }
+}
+
+Console.WriteLine("------------");
+
+// LINQ query for the above logic
+var results = from n in numbers
+              where n > 20
+              orderby n
+              select n;
+foreach (int number in results)
+{
+    Console.WriteLine(number);
+}
