2ea99984ec45f20c511c816a0ab30ff92e0519c0
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-08 11:57:00 +0300

    Added all three SQL queries into the example application.

--- JavaScript/Northwind-SQLite.js
+++ JavaScript/Northwind-SQLite.js
@@ -1,10 +1,40 @@
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('../Databases/northwind.db');
 
-const sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
-db.all(sql, (err, rows) => {
+listFinnishCustomers();
+listLondonEmployees();
+listTofuSales();
 
-    rows.forEach(row => {
-        console.log(row.CompanyName);
+function listFinnishCustomers() {
+    const sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
+    db.all(sql, (err, rows) => {
+
+        rows.forEach(row => {
+            console.log(row.CompanyName);
+        });
     });
-});
+}
+
+function listLondonEmployees() {
+    const sql = "SELECT * FROM Employees WHERE City = 'London'";
+    db.all(sql, (err, rows) => {
+
+        rows.forEach(row => {
+            console.log(row.FirstName + " " + row.LastName);
+        });
+    });
+}
+
+function listTofuSales() {
+    const sql = `SELECT SUM(UnitPrice * Quantity * (1-Discount)) AS TofuSales
+                 FROM [Order Details]
+                 WHERE ProductID IN (SELECT ProductID
+                                     FROM Products
+                                     WHERE ProductName LIKE '%Tofu%')`;
+    db.all(sql, (err, rows) => {
+
+        rows.forEach(row => {
+            console.log(row.TofuSales);
+        });
+    });
+}
