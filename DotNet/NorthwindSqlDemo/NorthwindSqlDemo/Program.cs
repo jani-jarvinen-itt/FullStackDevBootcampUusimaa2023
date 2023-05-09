a0c1be5b620cc0f7dfa5bc73909c5a54633b675a
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-09 09:20:16 +0300

    Created a sample application with C# to access a SQL Server database. Wrote code to read Finnish customers from the sample Northwind database.

---
+++ DotNet/NorthwindSqlDemo/NorthwindSqlDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+
+// open connection to the Northwind database
+string connectionString = "Server=localhost\\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;";
+SqlConnection conn = new(connectionString);
+conn.Open();
+
+// create an SQL statement
+string sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
+SqlCommand cmd = new(sql, conn);
+
+// execute the SQL and display results
+SqlDataReader reader = cmd.ExecuteReader();
+while (reader.Read())
+{
+    string companyName = reader["CompanyName"]?.ToString() ?? "";
+    Console.WriteLine(companyName);
+}
+
+// cleanup
+conn.Close();
