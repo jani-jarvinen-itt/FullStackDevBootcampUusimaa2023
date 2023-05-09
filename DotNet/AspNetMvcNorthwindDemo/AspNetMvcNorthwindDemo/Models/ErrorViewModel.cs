c92d904599071ddad2218d91bd948bb2f148235e
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-09 09:27:42 +0300

    Started to develop an ASP.NET MVC application for Northwind database access demo.

---
+++ DotNet/AspNetMvcNorthwindDemo/AspNetMvcNorthwindDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetMvcNorthwindDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
