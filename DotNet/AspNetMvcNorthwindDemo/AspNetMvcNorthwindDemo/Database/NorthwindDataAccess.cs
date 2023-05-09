ec12ce826b63127b4e8d7b44bc1025db4c347187
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-09 09:40:22 +0300

    Added the SQL NuGet package to the project and added C# code to access the Northwind database into a separate class.

---
+++ DotNet/AspNetMvcNorthwindDemo/AspNetMvcNorthwindDemo/Database/NorthwindDataAccess.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.SqlClient;
+
+namespace AspNetMvcNorthwindDemo.Database
+{
+    public class NorthwindDataAccess
+    {
+        public List<string> GetFinnishCustomers()
+        {
+            // open connection to the Northwind database
+            string connectionString = "Server=localhost\\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;";
+            SqlConnection conn = new(connectionString);
+            conn.Open();
+
+            // create an SQL statement
+            string sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
+            SqlCommand cmd = new(sql, conn);
+
+            // execute the SQL and display results
+            List<string> results = new();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string companyName = reader["CompanyName"]?.ToString() ?? "";
+                results.Add(companyName);
+            }
+
+            // cleanup
+            conn.Close();
+
+            // we are done
+            return results;
+        }
+    }
+}
