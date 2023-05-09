1c2e87cd122fbf42f55305b3ad3740b894f99f20
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-09 09:31:09 +0300

    Started adding a new view for Northwind customer data.

--- DotNet/AspNetMvcNorthwindDemo/AspNetMvcNorthwindDemo/Controllers/HomeController.cs
+++ DotNet/AspNetMvcNorthwindDemo/AspNetMvcNorthwindDemo/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace AspNetMvcNorthwindDemo.Controllers
             return View();
         }
 
+        public IActionResult Northwind()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
