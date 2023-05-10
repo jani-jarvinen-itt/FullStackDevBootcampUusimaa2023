f5b49c18f4ad12c96ea8e0d5b93b520d266beb87
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-10 15:19:54 +0300

    Added routing to the simple HTTP API controller and also implemented multiple endpoints.

--- DotNet/SimpleHttpApiDemo/Controllers/MySimpleApiController.cs
+++ DotNet/SimpleHttpApiDemo/Controllers/MySimpleApiController.cs
@@ -6,8 +6,21 @@ namespace SimpleHttpApiDemo.Controllers;
 [Route("[controller]")]
 public class MySimpleApiController : ControllerBase
 {
+    [Route("hello")]
     public string SayHello()
     {
         return "Hello World from a ASP.NET C# web API application.";
+    }
+
+    [Route("time")]
+    public DateTime CurrentTime()
+    {
+        return DateTime.Now;
+    }
+
+    [Route("numbers")]
+    public List<int> ReturnNumbers()
+    {
+        return new() { 9, 8, 7, 6, 5, 1, 2, 3, 4 };
     }
 }
