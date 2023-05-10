ff404796de3f242e05d450ea0b80abd9c45f0223
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-10 15:29:17 +0300

    Installed the System.Data.SqlClient NuGet package and added basic code to read all Northwind customers.

---
+++ DotNet/NorthwindHttpApi/Databases/NorthwindDataAccess.cs
@@ -0,0 +1,31 @@
+using System.Data.SqlClient;
+
+public class NorthwindDataAccess
+{
+    public List<string> GetAllCustomers()
+    {
+        // open connection to the Northwind database
+        string connectionString = "Server=localhost\\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;";
+        SqlConnection conn = new(connectionString);
+        conn.Open();
+
+        // create an SQL statement
+        string sql = "SELECT * FROM Customers";
+        SqlCommand cmd = new(sql, conn);
+
+        // execute the SQL and display results
+        List<string> results = new();
+        SqlDataReader reader = cmd.ExecuteReader();
+        while (reader.Read())
+        {
+            string companyName = reader["CompanyName"]?.ToString() ?? "";
+            results.Add(companyName);
+        }
+
+        // cleanup
+        conn.Close();
+
+        // we are done
+        return results;
+    }
+}
