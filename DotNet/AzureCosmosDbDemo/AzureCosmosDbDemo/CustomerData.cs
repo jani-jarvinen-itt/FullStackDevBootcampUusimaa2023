ca9639f3d7886d0d3b2e9cc2887c3b039c0f9c5b
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-10 10:18:53 +0300

    Implemented Cosmos DB item storage. Used a customer class as an example.

---
+++ DotNet/AzureCosmosDbDemo/AzureCosmosDbDemo/CustomerData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureCosmosDbDemo
+{
+    public class CustomerData
+    {
+        public string id { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
--- DotNet/AzureCosmosDbDemo/AzureCosmosDbDemo/Program.cs
+++ DotNet/AzureCosmosDbDemo/AzureCosmosDbDemo/Program.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.Azure.Cosmos;
+﻿using AzureCosmosDbDemo;
+using Microsoft.Azure.Cosmos;
 
 // connection properties
 string url = "https://fullstackbootcamp.documents.azure.com/";
 string primaryKey = "ggin9VzmcjzctgLyh9NGAUKHuG4EGbZnSroyUeptddNkDygZKSGOMJ2FJJ2nRR0B4vyFC5tSoY7vACDbTIeIZg==";
 string databaseName = "Jani_Testing";
+string containerName = "customers";
+string partitionKey = "/id";
 
 // create the client
 using CosmosClient client = new(url, primaryKey);
@@ -11,3 +14,22 @@ using CosmosClient client = new(url, primaryKey);
 // create or open the database
 var response = await client.CreateDatabaseIfNotExistsAsync(databaseName);
 Console.WriteLine("Database created or opened.");
+Database database = client.GetDatabase(databaseName);
+Console.WriteLine($"Database name: {database.Id}");
+
+// work with the container
+Container container = await database.CreateContainerIfNotExistsAsync(containerName, partitionKey);
+Console.WriteLine($"Opened container: {container.Id}");
+
+// create data to store
+CustomerData customer = new()
+{
+    id = "my-first-customer",
+    CustomerName = "My First Customer Inc.",
+    EmailAddress = "info@fistcustomer.com",
+    PhoneNumber = "123 456 789"
+};
+
+// store the object in the Cosmos DB container
+CustomerData createdItem = await container.CreateItemAsync(customer);
+Console.WriteLine($"Created item: {createdItem.id}");
