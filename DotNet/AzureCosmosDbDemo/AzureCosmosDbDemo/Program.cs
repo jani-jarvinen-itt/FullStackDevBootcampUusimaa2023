2dd0e7d6cd38bb15cc349e07f43dfbffcca2191f
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-10 09:42:00 +0300

    Started to implement Azure Cosmos DB demo application with C#. Database connection and client creation are already in place.

---
+++ DotNet/AzureCosmosDbDemo/AzureCosmosDbDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.Cosmos;
+
+// connection properties
+string url = "https://fullstackbootcamp.documents.azure.com/";
+string primaryKey = "ggin9VzmcjzctgLyh9NGAUKHuG4EGbZnSroyUeptddNkDygZKSGOMJ2FJJ2nRR0B4vyFC5tSoY7vACDbTIeIZg==";
+string databaseName = "Jani_Testing";
+
+// create the client
+using CosmosClient client = new(url, primaryKey);
+
+// create or open the database
+var response = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+Console.WriteLine("Database created or opened.");
