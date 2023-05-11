ec3abe2370d16b946bbfa2074f8a8f2f84fdd915
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-11 15:01:25 +0300

    Added a Fetch call to the JSONPlaceholder API.

--- React/my-app/src/UserList.js
+++ React/my-app/src/UserList.js
@@ -1,8 +1,25 @@
 import { useState } from 'react';
 
-function UserList()
-{
-    const [userList, setUserList] = useState([]);
+function UserList() {
+    const [userList, setUserList] = useState("");
+
+    console.log("Starting to fetch user data...");
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(json => {
+            console.log("Fetch of user data complete.");
+            console.log(json);
+
+            // loop through the users
+            let userNames = "";
+            for (let index = 0; index < json.length; index++) {
+                const userData = json[index];
+                userNames += userData.name + "\r\n";
+            }
+
+            // set the React component state -> causes screen to update
+            setUserList(userNames);
+        });
 
     return <div>
         <h1>This is the User List component!</h1>
