10da407500883cf2fd6150f914fbbaa710f9b3f5
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-11 10:19:12 +0300

    Added hello world text into the React application.

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello World version 2 from React!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
