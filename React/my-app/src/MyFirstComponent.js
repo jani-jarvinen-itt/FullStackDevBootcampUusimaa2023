9bd2f75ece44de493d95ad87f27a169e2a80f0ba
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-11 13:21:18 +0300

    Added a very simple React component to display static text.

---
+++ React/my-app/src/MyFirstComponent.js
@@ -0,0 +1,9 @@
+function MyFirstComponent()
+{
+    return <div>
+        <h1>This is the My First Component!</h1>
+        <p>This is normal text.</p>
+    </div>
+}
+
+export default MyFirstComponent;
--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import MyFirstComponent from './MyFirstComponent';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <MyFirstComponent />
+    <MyFirstComponent />
+    <MyFirstComponent />
   </React.StrictMode>
 );
 
