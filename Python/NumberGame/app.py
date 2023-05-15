ecdb6cfa718813f963ef7705505a44a6793f771a
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-15 14:56:31 +0300

    Added function to test the correct number against the player's guess. Also correctly referred to global variables.

--- Python/NumberGame/app.py
+++ Python/NumberGame/app.py
@@ -1,16 +1,37 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import random
 
 app = Flask(__name__)
 correct = 0
 
+
 @app.route("/")
 def hello():
     return "<html>Number Guessing Game API version 1.0</html>"
 
+
 @app.route("/api/startGame")
 def start_game():
+    global correct
     correct = random.randint(1, 20)
-    print(correct)
+    print("Game started, correct number is: " + str(correct))
     json = '{ "status": "Game started." }'
+    return Response(json, mimetype='application/json')
+
+
+@app.route("/api/checkGuess")
+def check_guess():
+    global correct
+    guess = int(request.args.get('guess', '0'))
+    # print(guess)
+
+    statusText = ""
+    if (guess == correct):
+        statusText = "Correct! You win the game!"
+    elif (guess < correct):
+        statusText = "The correct number is larger."
+    else:
+        statusText = "The correct number is smaller."
+
+    json = '{ "guess": "' + str(guess) + '", "status": "' + statusText + '" }'
     return Response(json, mimetype='application/json')
