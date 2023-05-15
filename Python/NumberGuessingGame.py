e3f68841185b931bd44d6f50abe1a376c4e17635
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-15 11:41:36 +0300

    Finalized the Python number guessing game logic.

--- Python/NumberGuessingGame.py
+++ Python/NumberGuessingGame.py
@@ -4,13 +4,21 @@ print("Welcome to the Number Guessing Game!")
 
 # select random number 1-20
 correct = random.randint(1, 20)
-print(correct)
+# print(correct)
 
 for round in range(3):
     # ask the player for a guess
     guess_value = input("Please enter your guess between 1 and 20: ")
     guess = int(guess_value)
+    # print("You guessed: " + str(guess))
 
-    print("You guessed: " + str(guess))
+    # is the guess correct?
+    if (guess == correct):
+        print("Correct! You win the game!")
+        break
+    elif (guess < correct):
+        print("The correct number is larger.")
+    else:
+        print("The correct number is smaller.")
 
 print("Game ends.")
