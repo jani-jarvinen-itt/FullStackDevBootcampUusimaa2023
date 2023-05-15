67d5bd1d7fc6a2b721b1a0ef2e966c828f5848d7
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-15 13:47:22 +0300

    Added a second Python Flask endpoint to the hello world application.

--- Python/HelloFlask/app.py
+++ Python/HelloFlask/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "<html>Hello, <b>World!</b></html>"
+
+@app.route("/numbers")
+def numbers():
+    return "<html>These are your lottery numbers: 2, 5, 17, 21, 22, 31, 34</html>"
