30c2ac6fce3f71ca655a08a787b4b8b08b05cbab
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-16 10:56:39 +0300

    Moved the database query processing into its own Python function.

--- Python/NorthwindSqLite.py
+++ Python/NorthwindSqLite.py
@@ -1,13 +1,17 @@
 import sqlite3
 
+def list_finnish_customer(con):
+    cur = con.cursor()
+    cur.execute("SELECT CompanyName FROM Customers WHERE Country = 'Finland'")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
 print("Starting to access the Northwind sample SQLite database.")
 con = sqlite3.connect("..\\Databases\\northwind.db")
 
-cur = con.cursor()
-cur.execute("SELECT * FROM Customers WHERE Country = 'Finland'")
-
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+list_finnish_customer(con)
 
 print("End.")
