6095268b09d008bdf1b12669b6c5ae83189ed0d7
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-17 10:59:30 +0300

    Added a Python example on how to filter CSV data.

---
+++ Python/FilterCsv.py
@@ -0,0 +1,17 @@
+import csv
+
+with open("Customers.csv") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            country = row[3]
+            if country == "France":
+                print(f'\tCompany {row[1]} is from the country {country}.')
+            line_count += 1
+    print(f'Processed {line_count} lines.')
+
+print("End.")
