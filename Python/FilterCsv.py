ecd0b3c8e8f4242be52cfe654b3d952d8d180751
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-05-17 13:01:12 +0300

    Added example of data validation during CSV file import in Python. The code now checks the company name column: it has to have a value.

--- Python/FilterCsv.py
+++ Python/FilterCsv.py
@@ -8,9 +8,11 @@ with open("Customers.csv") as csv_file:
             print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
-            country = row[3]
-            if country == "France":
-                print(f'\tCompany {row[1]} is from the country {country}.')
+            company_name = row[1]
+            if len(company_name) > 0:
+                country = row[3]
+                if country == "France":
+                    print(f'\tCompany {row[1]} is from the country {country}.')
             line_count += 1
     print(f'Processed {line_count} lines.')
 
